9b1dc0845f8a9b5f48d485f6d03ce05ac291ba9a
BartoszKarpowicz <155613703+BartoszKarpowicz@users.noreply.github.com>
2024-02-16 11:58:10 +0100

    Add files via upload

---
+++ mainwindow.cpp
@@ -0,0 +1,45 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QMessageBox>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    elementy.append(ui->spinBox->value());
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    int n = elementy.length();
+    for(int i=0; i<n; i++)
+        for(int j=1; j<n; j++) //pętla wewnętrzna
+            if(elementy[j-i]>elementy[j]) {
+                //zamiana miejscami
+                int temp = elementy[j-1];
+                elementy[j-1] = elementy[j];
+                elementy[j] = temp;
+                //swap(tap[j-1], tab[j]);
+            }
+    QString elementyStr = "";
+    QString el;
+    for (int i=0; i<n; i++) {
+        el = QString::number(elementy[i]);
+        elementyStr += el + " ";
+    }
+    QMessageBox msg(this);
+    msg.setText(elementyStr);
+    msg.setStandardButtons(QMessageBox::Ok);
+    msg.setWindowTitle("Posortowanie liczby");
+        msg.exec();
+}
